63ba1f0c838f2fddf4799122231167ec0d73b34d
Yoselyn Cántaro <yoselynespinoza27@gmail.com>
2024-10-22 12:29:07 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/pe/com/cibertec/lp2_carrito_compra/Lp2CarritoCompraApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.lp2_carrito_compra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2CarritoCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2CarritoCompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/lp2_carrito_compra/Lp2CarritoCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.lp2_carrito_compra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2CarritoCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
